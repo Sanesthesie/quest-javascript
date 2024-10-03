ac58191bfc05d50048c2b2110e0e39b855cd1c7e
Victor <155189704+Sanesthesie@users.noreply.github.com>
2024-10-03 17:16:54 +0200

    Create first-script.js
    Solution à la quête First Script

---
+++ first-script.js
@@ -0,0 +1,13 @@
+const a = 3
+const b = 6
+const c = 13
+
+    if (a === b && a === c) {
+        console.log("Les trois variables sont identiques")
+    }
+    else if (a === b || a === c || b ===c) {
+        console.log("Deux des variables sont identiques")   
+    }
+    else { 
+        console.log("Toutes les variables sont différentes")   
+    }
